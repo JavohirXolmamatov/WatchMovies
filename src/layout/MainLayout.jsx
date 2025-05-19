dcae077fc92ad5cb6b1e195497e30ff1372ed576
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2025-05-19 13:43:25 +0500

    added scrolltotop

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 function MainLayout() {
   return (
     <div className="w-full h-full">
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
